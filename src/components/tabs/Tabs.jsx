9049aba0c98794e29d4bd0f3211b092cd2159d19
dawit tekle <dvwish9214@gmail.com>
2024-04-09 07:34:44 +0300

    border tab and defautl update

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -7,7 +7,6 @@ import PropTypes from "prop-types";
 const TabContext = createContext();
 const useStyles = makeStyles((theme) => ({
   tabsContainer: {
-    borderBottom: `1px solid #D3D3D3`,
     marginBottom: theme.spacing(2)
   }
 }));
@@ -16,7 +15,7 @@ function Tabs({ children, activeTab = 0, handleTabChange }) {
   const classes = useStyles();
   return (
     <TabContext.Provider value={{ activeTab, handleTabChange }}>
-      <Box className={classes.tabsContainer}>
+      <Box className={classes.tabsContainer} sx={{ borderBottom: 1, borderColor: "divider" }}>
         <MuiTabs value={activeTab} onChange={handleTabChange} indicatorColor="primary">
           {React.Children.map(children, (child, index) => {
             return React.cloneElement(child, { index });
--- src/pages/ContentManager.jsx
+++ src/pages/ContentManager.jsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function ContentManager() {
-  const [activeTab, setActiveTab] = React.useState(0);
+  const [activeTab, setActiveTab] = React.useState(1);
   const { state } = useGlobalSetting();
   const { labels } = state.setting;
   const [modelOpen, setModelOpen] = useState(false);
