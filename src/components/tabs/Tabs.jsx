da805b5979b5d932cfe8480995a556bd8ee0d98f
dawit tekle <dvwish9214@gmail.com>
2024-04-09 07:47:35 +0300

    tab update

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -7,7 +7,7 @@ import PropTypes from "prop-types";
 const TabContext = createContext();
 const useStyles = makeStyles((theme) => ({
   tabsContainer: {
-    marginBottom: theme.spacing(2)
+    marginBottom: theme.spacing(4)
   }
 }));
 
