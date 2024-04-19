c92c147265ddea8e1ce75d93440d244e578cc416
Hermyh21 <128360630+Hermyh21@users.noreply.github.com>
2024-04-19 04:33:31 -0700

    admin settings page created (#13)
    * imported AdminSettings to App.jsx
    
    * admin settings page created and imported

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Layout from "~/layouts";
 import AdminLayout from "~/layouts/AdminLayout";
 import ProtectedLayout from "~/layouts/ProtectedLayout";
 import NotFoundPage from "~/pages/404";
+import AdminSettings from "~/pages/AdminSettings";
 import ContentManager from "~/pages/ContentManager";
 import Login from "~/pages/Login";
 import CategoryPages from "~/pages/WebsiteCategoryPages";
@@ -32,6 +33,7 @@ function App() {
 
                 <Route exact path="/secure" element={<ProtectedLayout />}>
                   <Route path="content_manager" element={<ContentManager />} />
+                  <Route path="admin_settings" exact element={<AdminSettings />} />
                 </Route>
               </Route>
             </Routes>
