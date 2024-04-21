0b15436205a1d4d88ea54f6ff8bfd1968b02e1a6
kuldep7 <kuldep7@gmail.com>
2024-04-21 23:41:54 +0530

    Refactor GridView component with useEffect and useState hooks, fetch data dynamically based on route parameters, and display loading state.
    - Replaced useGridData hook with useEffect and useState
    - Fetched data based on dynamic route parameters
    - Displayed loading state during data fetches
    - Handled empty data response

--- src/layouts/GridView.jsx
+++ src/layouts/GridView.jsx
@@ -1,8 +1,11 @@
-import { Grid } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { useEffect, useState } from "react";
 
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { useParams } from "react-router-dom";
+
+import { axiosPrivate } from "~/_api";
 import CardView from "~/components/Card";
-import useGridData from "~/hooks/useGridData";
 
 import config from "../constants/endpoints.json";
 
@@ -20,11 +23,76 @@ const useStyles = makeStyles(() => ({
 
 export default function GridView() {
   const classes = useStyles();
-  const { cardData } = useGridData(currentConfig.contentData);
+  const { category = "home" } = useParams();
+  const [cardData, setCardData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  const fetchData = () => {
+    axiosPrivate
+      .get(`/api/${currentConfig.contentData}`, {
+        params: {
+          content_category: category,
+          lang: "english"
+        }
+      })
+      .then(({ data }) => {
+        setCardData(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("error :>> ", err);
+        setLoading(false);
+      });
+  };
+
+  useEffect(fetchData, [category]);
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          height: "calc(100dvh - 7.25rem)",
+          width: "100%",
+          flexGrow: 1,
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (!loading && cardData.length === 0) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          height: "calc(100dvh - 7.25rem)",
+          width: "100%",
+          flexGrow: 1,
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "2rem",
+            fontWeight: 500,
+            color: "text.secondary"
+          }}
+        >
+          Unable to Fetch Data
+        </Typography>
+      </Box>
+    );
+  }
   return (
     <Grid container className={classes.grid}>
-      {cardData &&
+      {!loading &&
+        cardData.length > 0 &&
         cardData.map((data) => (
           <Grid item xs={12} sm={6} lg={4} key={data.id}>
             <CardView data={data} />
