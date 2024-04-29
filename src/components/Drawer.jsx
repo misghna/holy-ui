65548205effc11b88eb914b23f90f289629a4177
sirak kidane <sirakkidane12345@gmail.com>
2024-04-29 21:04:03 +0300

    added sign in button to the side bar

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useMemo, useState } from "react";
-
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
@@ -12,12 +11,13 @@ import {
   SwipeableDrawer,
   Divider,
   ListItemButton,
+  Button,
   Box,
-  Typography
+  Typography,
+  IconButton,
+  ListSubheader
 } from "@mui/material";
 import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import ListSubheader from "@mui/material/ListSubheader";
 import { styled, useTheme } from "@mui/material/styles";
 import { bool, func } from "prop-types";
 import { useNavigate } from "react-router-dom";
@@ -25,6 +25,7 @@ import { useNavigate } from "react-router-dom";
 import { DRAWER_WIDTH } from "~/constants/theme";
 import { useGlobalSetting } from "~/contexts/GlobalSettingProvider";
 import { useLayout } from "~/contexts/LayoutProvider";
+import Login from "~/pages/Login";
 
 export const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -35,7 +36,6 @@ export const DrawerHeader = styled("div")(({ theme }) => ({
   zIndex: 10,
   width: "100%",
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
   justifyContent: "flex-end",
   borderBottom: `1px solid ${theme.palette.primary.main}`,
@@ -44,15 +44,12 @@ export const DrawerHeader = styled("div")(({ theme }) => ({
 
 const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, drawerAlwaysOpen }) {
   const { state: drawerState } = useLayout();
-
   const { open } = drawerState;
-
   const theme = useTheme();
   const { setting } = useGlobalSetting();
-
   const navigate = useNavigate();
-
   const menu = useMemo(() => setting?.menu || [], [setting?.menu]);
+  const isAuthenticated = setting.authenticated;
 
   const groupedMenu = useMemo(
     () =>
@@ -66,12 +63,9 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
   const submenus = useMemo(() => {
     const rows = Object.keys(groupedMenu).length;
     const columns = Object.values(groupedMenu).reduce((max, group) => Math.max(max, group.length), 0);
-
     const nestedArray = Array.from({ length: rows }, () => Array.from({ length: columns }, () => false));
-
     return nestedArray;
   }, [groupedMenu]);
-
   const [isSubMenOpen, setIsSubMenOpen] = useState([...submenus]);
 
   const renderSubMenu = (submenu, typeIndex, menuIndex) => {
@@ -81,12 +75,7 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
         <List disablePadding>
           {submenu &&
             submenu.map((item) => (
-              <ListItemButton
-                key={item.name}
-                onClick={() => {
-                  navigate(item.url, { replace: true });
-                }}
-              >
+              <ListItemButton key={item.name} onClick={() => navigate(item.url, { replace: true })}>
                 <ListItemIcon>{item.icon}</ListItemIcon>
                 <ListItemText primary={item.name} />
               </ListItemButton>
@@ -96,39 +85,37 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
       </Collapse>
     );
   };
+
   const showMoreOrLessIcon = (submenu, typeIndex, menuIndex) => {
     if (submenu.length === 0) return null;
     if (isSubMenOpen.length > 0 && isSubMenOpen[typeIndex][menuIndex])
-      return (
-        <ExpandLessIcon
-          onClick={() => {
-            handleSubMenCloseClick(typeIndex, menuIndex);
-          }}
-        />
-      );
-    return (
-      <ExpandMoreIcon
-        onClick={() => {
-          handleSubMenOpenClick(typeIndex, menuIndex);
-        }}
-      />
-    );
+      return <ExpandLessIcon onClick={() => handleSubMenCloseClick(typeIndex, menuIndex)} />;
+    return <ExpandMoreIcon onClick={() => handleSubMenOpenClick(typeIndex, menuIndex)} />;
   };
+
   const handleSubMenOpenClick = (typeIndex, menuIndex) => {
     const arr = [...submenus];
     let typeOpens = arr[typeIndex];
     typeOpens = typeOpens.map(() => false);
     typeOpens[menuIndex] = true;
     arr[typeIndex] = [...typeOpens];
-
     setIsSubMenOpen(arr);
   };
+
   const handleSubMenCloseClick = (typeIndex, menuIndex) => {
     const arr = [...submenus];
     let typeOpens = arr[typeIndex];
     typeOpens[menuIndex] = false;
     arr[typeIndex] = [...typeOpens];
     setIsSubMenOpen(arr);
+  };
+
+  const StyledButton = styled(Button)({
+    marginTop: "16px"
+  });
+
+  const handleNavigation = (path) => {
+    navigate(path);
   };
 
   return (
@@ -147,8 +134,6 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
       variant="persistent"
       anchor="left"
       open={open}
-      onClose={handleDrawerClose}
-      onOpen={() => {}}
     >
       <DrawerHeader>
         <Box sx={{ width: "100%", backgroundColor: "white" }} display="flex" alignItems="center">
@@ -162,7 +147,7 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
           )}
         </Box>
       </DrawerHeader>
-      <List>
+      <List style={{ marginBottom: "64px" }}>
         {groupedMenu &&
           Object.entries(groupedMenu).map((items, typeIndex) => (
             <Fragment key={items?.at(0)}>
@@ -184,7 +169,6 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
                   >
                     <ListItemIcon>{item.icon}</ListItemIcon>
                     <ListItemText primary={item.name} />
-
                     {item.sub_menu && showMoreOrLessIcon(item.sub_menu, typeIndex, menuIndex)}
                   </ListItem>
                   {item.sub_menu && renderSubMenu(item.sub_menu, typeIndex, menuIndex)}
@@ -193,6 +177,24 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
             </Fragment>
           ))}
       </List>
+      <Divider className="home" style={{ paddingTop: "10px" }} />
+
+      <Button
+        onClick={() => handleNavigation(!isAuthenticated ? "/login" : "")}
+        variant="contained"
+        color="primary"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          width: 225,
+          zIndex: 999,
+          background: "white",
+          color: "black",
+          border: "1px solid black"
+        }}
+      >
+        {!isAuthenticated ? "Login" : "lOgin"}
+      </Button>
     </SwipeableDrawer>
   );
 });
