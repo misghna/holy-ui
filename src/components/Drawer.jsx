9d4f8bfe86ce08b3815dd10465647e3023cb64b7
Sirak Kidane <75301678+siru-kid@users.noreply.github.com>
2024-05-04 18:52:21 +0300

    this the new updated branch of drawer.jsx (#28)
    Co-authored-by: sirak kidane <sirakkidane12345@gmail.com>

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useMemo, useState } from "react";
-
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
@@ -12,19 +11,20 @@ import {
   SwipeableDrawer,
   Divider,
   ListItemButton,
+  Button,
   Box,
-  Typography
+  Typography,
+  IconButton,
+  ListSubheader
 } from "@mui/material";
 import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import ListSubheader from "@mui/material/ListSubheader";
 import { styled, useTheme } from "@mui/material/styles";
 import { bool, func } from "prop-types";
 import { useNavigate } from "react-router-dom";
-
 import { DRAWER_WIDTH } from "~/constants/theme";
 import { useGlobalSetting } from "~/contexts/GlobalSettingProvider";
 import { useLayout } from "~/contexts/LayoutProvider";
+import Login from "~/pages/Login";
 
 export const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -34,25 +34,31 @@ export const DrawerHeader = styled("div")(({ theme }) => ({
   left: 0,
   zIndex: 10,
   width: "100%",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
+  padding: theme.spacing(1),
   ...theme.mixins.toolbar,
   justifyContent: "flex-end",
   borderBottom: `1px solid ${theme.palette.primary.main}`,
   backgroundColor: "white"
 }));
 
+const StyledCustomButtons = styled(Button)({
+  width: "100%",
+  height: 30,
+  margin: "0 auto",
+  zIndex: 15,
+  background: "white",
+  color: "black",
+  border: "1px solid black"
+});
+
 const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, drawerAlwaysOpen }) {
   const { state: drawerState } = useLayout();
-
   const { open } = drawerState;
-
   const theme = useTheme();
   const { setting } = useGlobalSetting();
-
   const navigate = useNavigate();
-
   const menu = useMemo(() => setting?.menu || [], [setting?.menu]);
+  const isAuthenticated = setting.authenticated;
 
   const groupedMenu = useMemo(
     () =>
@@ -63,72 +69,59 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
       }, {}),
     [menu]
   );
+
   const submenus = useMemo(() => {
     const rows = Object.keys(groupedMenu).length;
     const columns = Object.values(groupedMenu).reduce((max, group) => Math.max(max, group.length), 0);
-
     const nestedArray = Array.from({ length: rows }, () => Array.from({ length: columns }, () => false));
-
     return nestedArray;
   }, [groupedMenu]);
 
   const [isSubMenOpen, setIsSubMenOpen] = useState([...submenus]);
 
   const renderSubMenu = (submenu, typeIndex, menuIndex) => {
-    if (submenu?.length == 0 || isSubMenOpen.length === 0) return null;
+    if (submenu?.length === 0 || isSubMenOpen.length === 0) return null;
+
     return (
       <Collapse in={isSubMenOpen[typeIndex][menuIndex]} timeout="auto" unmountOnExit>
         <List disablePadding>
-          {submenu &&
-            submenu.map((item) => (
-              <ListItemButton
-                key={item.name}
-                onClick={() => {
-                  navigate(item.url, { replace: true });
-                }}
-              >
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.name} />
-              </ListItemButton>
-            ))}
+          {submenu.map((item) => (
+            <ListItemButton key={item.name} onClick={() => navigate(item.url, { replace: true })}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          ))}
         </List>
         <Divider />
       </Collapse>
     );
   };
+
   const showMoreOrLessIcon = (submenu, typeIndex, menuIndex) => {
     if (submenu.length === 0) return null;
-    if (isSubMenOpen.length > 0 && isSubMenOpen[typeIndex][menuIndex])
-      return (
-        <ExpandLessIcon
-          onClick={() => {
-            handleSubMenCloseClick(typeIndex, menuIndex);
-          }}
-        />
-      );
-    return (
-      <ExpandMoreIcon
-        onClick={() => {
-          handleSubMenOpenClick(typeIndex, menuIndex);
-        }}
-      />
+
+    return isSubMenOpen.length > 0 && isSubMenOpen[typeIndex][menuIndex] ? (
+      <ExpandLessIcon onClick={() => handleSubMenCloseClick(typeIndex, menuIndex)} />
+    ) : (
+      <ExpandMoreIcon onClick={() => handleSubMenOpenClick(typeIndex, menuIndex)} />
     );
   };
+
   const handleSubMenOpenClick = (typeIndex, menuIndex) => {
     const arr = [...submenus];
-    let typeOpens = arr[typeIndex];
-    typeOpens = typeOpens.map(() => false);
-    typeOpens[menuIndex] = true;
-    arr[typeIndex] = [...typeOpens];
-
+    arr[typeIndex] = arr[typeIndex].map(() => false);
+    arr[typeIndex][menuIndex] = true;
     setIsSubMenOpen(arr);
   };
+
   const handleSubMenCloseClick = (typeIndex, menuIndex) => {
     const arr = [...submenus];
-    let typeOpens = arr[typeIndex];
-    typeOpens[menuIndex] = false;
-    arr[typeIndex] = [...typeOpens];
+    arr[typeIndex][menuIndex] = false;
     setIsSubMenOpen(arr);
+  };
+
+  const handleNavigation = (path) => {
+    navigate(path);
   };
 
   return (
@@ -147,12 +140,14 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
       variant="persistent"
       anchor="left"
       open={open}
-      onClose={handleDrawerClose}
-      onOpen={() => {}}
     >
       <DrawerHeader>
-        <Box sx={{ width: "100%", backgroundColor: "white" }} display="flex" alignItems="center">
-          <Typography variant="h6" textAlign="center" noWrap flexGrow={1} sx={{ width: "100%" }}>
+        <Box
+          sx={{ width: "100%", backgroundColor: "white", display: "flex", justifyContent: "center" }}
+          display="flex"
+          alignItems="center"
+        >
+          <Typography variant="h6" textAlign="center" noWrap flexGrow={1}>
             Holy-UI
           </Typography>
           {!drawerAlwaysOpen && (
@@ -162,37 +157,50 @@ const SideMenuDrawer = React.memo(function SideMenuDrawer({ handleDrawerClose, d
           )}
         </Box>
       </DrawerHeader>
-      <List>
-        {groupedMenu &&
-          Object.entries(groupedMenu).map((items, typeIndex) => (
-            <Fragment key={items?.at(0)}>
-              <ListSubheader>{items?.at(0) === "public" ? "Public" : "Secure"}</ListSubheader>
-              <Divider />
-              {items?.at(1)?.map((item, menuIndex) => (
-                <>
-                  <ListItem
-                    key={item.name}
-                    button
-                    onClick={() => {
-                      if (item.sub_menu.length > 0 && !isSubMenOpen[typeIndex][menuIndex]) {
-                        handleSubMenOpenClick(typeIndex, menuIndex);
-                      }
-                      if (item.sub_menu.length == 0) {
-                        navigate(item.url);
-                      }
-                    }}
-                  >
-                    <ListItemIcon>{item.icon}</ListItemIcon>
-                    <ListItemText primary={item.name} />
 
-                    {item.sub_menu && showMoreOrLessIcon(item.sub_menu, typeIndex, menuIndex)}
-                  </ListItem>
-                  {item.sub_menu && renderSubMenu(item.sub_menu, typeIndex, menuIndex)}
-                </>
-              ))}
-            </Fragment>
-          ))}
-      </List>
+      {/* Added container with fixed height and overflow */}
+      <Box style={{ overflow: "auto", maxHeight: "calc(100vh - 64px)" }}>
+        <List style={{ padding: "2px 0" }}>
+          {groupedMenu &&
+            Object.entries(groupedMenu).map(([type, items], typeIndex) => (
+              <Fragment key={type}>
+                <ListSubheader sx={{ fontWeight: "bold" }}>{type === "public" ? "Public" : "Secure"}</ListSubheader>
+                <Divider sx={{ marginBottom: 1 }} />
+                {items.map((item, menuIndex) => (
+                  <Fragment key={item.name}>
+                    <ListItem
+                      button
+                      dense
+                      onClick={() => {
+                        if (item.sub_menu.length > 0 && !isSubMenOpen[typeIndex][menuIndex]) {
+                          handleSubMenOpenClick(typeIndex, menuIndex);
+                        }
+                        if (item.sub_menu.length === 0) {
+                          navigate(item.url);
+                        }
+                      }}
+                      sx={{ py: 1 }} // Adjusted vertical padding
+                    >
+                      <ListItemIcon>{item.icon}</ListItemIcon>
+                      <ListItemText primary={item.name} />
+                      {item.sub_menu && showMoreOrLessIcon(item.sub_menu, typeIndex, menuIndex)}
+                    </ListItem>
+                    {item.sub_menu && renderSubMenu(item.sub_menu, typeIndex, menuIndex)}
+                  </Fragment>
+                ))}
+              </Fragment>
+            ))}
+          {!isAuthenticated ? (
+            <StyledCustomButtons onClick={() => handleNavigation("/login")} color="primary">
+              Login
+            </StyledCustomButtons>
+          ) : (
+            <StyledCustomButtons onClick={() => handleNavigation("/logout")} color="primary">
+              Logout
+            </StyledCustomButtons>
+          )}
+        </List>
+      </Box>
     </SwipeableDrawer>
   );
 });
